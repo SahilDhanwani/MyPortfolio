a9ae8a0eb11b88b1d616f0fea3211c0ec4db4d65
SahilDhanwani <sahildhanwani291203@gmail.com>
2024-01-20 23:32:31 +0530

    Add margin to textarea.form-control-lg and create new CSS classes for project page and card links. Also, add form validation functions to main.js.

--- main.js
+++ main.js
@@ -1,6 +1,47 @@
-console.log("Hello World!");
-fullname = "John Doe";
-console.log(fullname);
-let a = 5;
-let b = '5';
-console.log(a==b);
+function validateForm() {
+    var first_name = document.contact.first_name.value;
+    var last_name = document.contact.last_name.value;
+    var email = document.contact.value;
+    var thoughts = document.contact.thoughts.value;
+
+    if (first_name_validation(first_name)) {
+        if (last_name_validation(last_name)) {
+            if (email_validation(email)) {
+                    return true;
+            }
+        }
+    }
+    return false;
+}
+
+function first_name_validation(first_name) {
+    var first_name_len = first_name.length;
+    var letters = /^[A-Za-z]+$/;
+    if (first_name_len == 0 || !first_name.match(letters)) {
+        alert("First name should not be empty & shold have only letters");
+        first_name.focus();
+        return false;
+    }
+    return true;
+}
+
+function last_name_validation(last_name) {
+    var last_name_len = last_name.length;
+    var letters = /^[A-Za-z]+$/;
+    if (last_name_len == 0 || !last_name.match(letters)) {
+        alert("Last name should not be empty & shold have only letters");
+        last_name.focus();
+        return false;
+    }
+    return true;
+}
+
+function email_validation(email) {
+    var email_len = email.length;
+    if (email_len == 0) {
+        alert("Email should not be empty");
+        email.focus();
+        return false;
+    }
+    return true;
+}
